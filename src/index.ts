e99cdd1868ddcee8c829553396a2740ba2ed2433
SamuelFlet <samuelrhfletcher@hotmail.com>
2022-08-26 18:44:37 -0400

    added some things

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from "apollo-server";
-// import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 import { schema } from "./schema";
 import { context } from "./context";
@@ -8,6 +8,7 @@ export const server = new ApolloServer({
   schema,
   context,
   introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
 const port = process.env.PORT || 3000;
