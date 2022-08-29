62c09967f4c364ee06d1173a52830c3d51b6e937
SamuelFlet <samuelrhfletcher@hotmail.com>
2022-08-29 17:04:26 -0400

    wd

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import { ApolloServer } from "apollo-server-azure-functions";
+import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-
+import express from "express";
 import { schema } from "./schema";
 import { context } from "./context";
+import http from "http";
 
-const server = new ApolloServer({
-  schema,
-  context,
-  introspection: true,
-  plugins: [ApolloServerPluginLandingPageLocalDefault()],
-});
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    schema,
+    context,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
 
-exports.graphqlHandler = server.createHandler();
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+startApolloServer();
