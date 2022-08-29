fa0361ec27e6cf4330d1a8674f11ff6170a9df4b
SamuelFlet <samuelrhfletcher@hotmail.com>
2022-08-29 17:39:27 -0400

    fd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { ApolloServer } from "apollo-server-azure-functions";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { schema } from "./schema";
 import { context } from "./context";
 
@@ -7,7 +6,6 @@ const server = new ApolloServer({
   schema,
   context,
   introspection: true,
-  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
 exports.graphqlHandler = server.createHandler();
